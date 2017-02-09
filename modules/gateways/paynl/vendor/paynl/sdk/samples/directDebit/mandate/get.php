4f329920d9ffe64a49116fabdb0078de23123e5b
Andy Pieters <andy@pay.nl>
2017-02-09 16:35:54 +0100

    Removed vendor folder from source

--- modules/gateways/paynl/vendor/paynl/sdk/samples/directDebit/mandate/get.php
+++
@@ -1,9 +0,0 @@
-<?php
-require_once '../../../vendor/autoload.php';
-require_once '../../config.php';
-try {
-    $result = Paynl\DirectDebit\Mandate::get('IO-6604-2112-1710');
-    var_dump($result->getData()['result']);
-} catch (\Paynl\Error\Error $e){
-    echo "Error: ".$e->getMessage();
-}
