bbce06ee63b5734d5ccbe4896e2eac081d0586df
woutse <wouter@pay.nl>
2020-10-30 16:58:11 +0100

    Fix for VVVgiftcard

--- modules/gateways/paynl_vvvgiftcard.php
+++ modules/gateways/paynl_vvvgiftcard.php
@@ -6,13 +6,13 @@ if (!defined("WHMCS")) {
 require_once __DIR__."/paynl/vendor/autoload.php";
 
 function paynl_vvvgiftcard_MetaData(){
-    return \Paynl\Whmcs\Gateway\Webshopgiftcard::getMetaData();
+    return \Paynl\Whmcs\Gateway\VVVGiftcard::getMetaData();
 }
 
 function paynl_vvvgiftcard_config(){
-    return \Paynl\Whmcs\Gateway\Webshopgiftcard::getConfig();
+    return \Paynl\Whmcs\Gateway\VVVGiftcard::getConfig();
 }
 
 function paynl_vvvgiftcard_link($params){
-    return \Paynl\Whmcs\Gateway\Webshopgiftcard::getLink($params);
+    return \Paynl\Whmcs\Gateway\VVVGiftcard::getLink($params);
 }
